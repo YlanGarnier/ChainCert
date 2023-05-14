ace15013595a548b9ccb2b162d7b4266aa1693e9
Lenny Vongphouthone <lenny.vongphouthone@epitech.eu>
2023-05-14 06:39:47 +0200

    add: contract chainCert deployed and remove taqueria

---
+++ ChainCert/contracts/config.sh
@@ -0,0 +1,3 @@
+octez-client --endpoint https://rpc.ghostnet.teztnets.xyz config update
+export OCTEZ_CLIENT_UNSAFE_DISABLE_DISCLAIMER=yes
+octez-client import secret key me unencrypted:edskRwy5iXr5LkvQeN2gqseABj8RQHo9aep2PifEr9FXfovxyp4xq4WeS3pQyJ2F3nsJrcLGW4bbhT2c4m2iUNmbQNK4VSMSaP --force
