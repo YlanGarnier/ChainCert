e193331cc43676752330359dd35076f6594577db
Nestyles <tom.sancho@epitech.eu>
2023-05-14 13:33:16 +0200

    [FEAT] Better navigation with a login button and create an account

--- ChainCert-Front/src/components/navbar/navbar.tsx
+++ ChainCert-Front/src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import './navbar.scss';
 
 const Navbar = () => {
+  const navigate = useNavigate();
 
   return (
     <div className='navbarWrapper'>
       <img className="navbarLogo" src={logo}></img>
-      <h1 className='navbarLogin'> Login </h1>
+      <h1 className='navbarLogin' onClick={() => navigate("/login")}> Login </h1>
     </div >
   );
 }
